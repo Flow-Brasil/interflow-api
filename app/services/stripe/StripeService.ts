61e801bc51cf21f4fa19f09538903dfa97042962
FelipeRibeiroLabs <frlabsmd@gmail.com>
2023-02-27 08:15:51 -0300

    update stripe return json

--- app/services/stripe/StripeService.ts
+++ app/services/stripe/StripeService.ts
@@ -3,49 +3,50 @@ import { User } from "@models/users/User";
 import { Stripe } from "stripe";
 const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY, {
   apiVersion: "2022-11-15",
-  typescript: true
+  typescript: true,
 });
 
 const userRepository = sequelize.getRepository(User);
 
 class StripeService {
-    async createPaymentIntent(userId: string, amount: number, currency: string) {
-        const user = await userRepository.findByPk(userId);
-        if(!user) {
-          return {message: "User does not exist"}
-        }
-        
-        try {
-        let costumers = await stripe.customers.list()
-        let costumer = costumers.data.find((costumer) => costumer.email === user.dataValues.email)
+  async createPaymentIntent(userId: string, amount: number, currency: string) {
+    const user = await userRepository.findByPk(userId);
+    if (!user) {
+      return { message: "User does not exist" };
+    }
 
-        console.log("COSTUMERS", costumer)
-        
-        if (!costumer) {
-            costumer = await stripe.customers.create({
-                name: user.dataValues.nickname,
-                email: user.dataValues.email,
-                address: {
-                },
-            });
-        }
+    try {
+      let costumers = await stripe.customers.list();
+      let costumer = costumers.data.find(
+        (costumer) => costumer.email === user.dataValues.email
+      );
 
-        const ephemeralKey = await stripe.ephemeralKeys.create(
-            { customer: costumer.id },
-            { apiVersion: "2022-11-15" }
-        );
+      console.log("COSTUMERS", costumer);
 
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: amount,
-            currency: currency,
-            customer: costumer.id,
-            payment_method_types: ["card"],
+      if (!costumer) {
+        costumer = await stripe.customers.create({
+          name: user.dataValues.nickname,
+          email: user.dataValues.email,
+          address: {},
         });
+      }
+
+      const ephemeralKey = await stripe.ephemeralKeys.create(
+        { customer: costumer.id },
+        { apiVersion: "2022-11-15" }
+      );
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: currency,
+        customer: costumer.id,
+        payment_method_types: ["card"],
+      });
 
       return {
         paymentIntent: paymentIntent.client_secret,
         ephemeralKey: ephemeralKey.secret,
-        customer: costumer.id
+        customer: costumer.id,
       };
     } catch (err: any) {
       console.log("ERROR ---", err);
@@ -56,21 +57,20 @@ class StripeService {
   async addTokensToUser(userId: string, amount: number) {
     const user = await userRepository.findByPk(userId);
 
-    if(!user) {
-      return {message: "User does not exist"}
+    if (!user) {
+      return { message: "User does not exist" };
     }
 
-    try{
-        await user.update({
-            interflowTokens: user.dataValues.interflowTokens + amount
-        })
+    try {
+      await user.update({
+        interflowTokens: user.dataValues.interflowTokens + amount,
+      });
 
-        return user;
+      return user;
     } catch (err: any) {
-        console.log("ERROR ---", err);
-        return err;
-    }    
-
+      console.log("ERROR ---", err);
+      return err;
+    }
   }
 }
 
